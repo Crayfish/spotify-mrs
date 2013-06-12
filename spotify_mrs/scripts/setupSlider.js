0a740e6020889e4d22b4e0e3cdb27414bbe4dd28
julius.gruber <julius.gruber@chello.at>
2013-06-12 20:08:11 +0000

    Song Hotness Slider

--- spotify_mrs/scripts/setupSlider.js
+++ spotify_mrs/scripts/setupSlider.js
@@ -2,34 +2,42 @@ require([
   //'$api/models',
   //'$views/image#Image',
   //'$views/popup#Popup'
-  'scripts/echonest'
+  'scripts/echonestDynamic'
 ], function(echonest) {
   'use strict';
 
        
-  var setUpPopSlider = function setUpPopSlider(echonest){
-          setUpPopSlider1(echonest);
+  var setUpPopSlider = function setUpPopSlider(echonestDynamic){
+          setUpPopSlider1(echonestDynamic);
        
         };
  
  
  
  
-  var setUpHotslider = function setUpHotSlider(echonest){
-          setUpHotSlider1(echonest);
+  var setUpHotslider = function setUpHotSlider(echonestDynamic){
+          setUpHotSlider1(echonestDynamic);
          
         };
+        
+        
+        var setUpSongHotSlider = function setUpHotSlider(echonestDynamic){
+        	setUpSongHotSlider1(echonestDynamic);
+           
+          };
+        
+        
  
  
   exports.setUpHotSlider = setUpHotslider;
   exports.setUpPopSlider = setUpPopSlider;
- 
+  exports.setUpSongHotSlider=setUpSongHotSlider;
  
 });
 
 
 
-function setUpPopSlider1(echonest){
+function setUpPopSlider1(echonestDynamic){
           console.log('SetUpPopSlider() betreten');
                 //jQuery Syntax: $(selector).action(), # steht für element with id="slider-range"
                 $( "#slider-pop" ).slider({
@@ -55,12 +63,14 @@ function setUpPopSlider1(echonest){
                 },
                
                 stop: function ( event, ui ) {
+                	
+                	echonestDynamic.changeArtistPopularity();
                         //console.log("Pop slider Stop");
-                	if(document.getElementById('song').checked){
-                        echonest.getPlaylistSongSimilarity();
+                	/*if(document.getElementById('song').checked){
+                        echonestDynamic.changeArtistPopularity();
                 	}else{
-                		echonest.getPlaylistArtistSimilarity();
-                	}
+                		 echonestDynamic.changeArtistPopularity();
+                	}*/
                 },
                
                 /*change: function( event, ui ) {
@@ -75,8 +85,8 @@ function setUpPopSlider1(echonest){
                 " - " + $( "#slider-pop" ).slider( "values", 1 ) );
 }
 
-function setUpHotSlider1(echonest){
-        console.log('SetUpHotSlider() betreten');
+function setUpHotSlider1(echonestDynamic){
+        console.log('SetUpSongHotSlider() betreten');
         //jQuery Syntax: $(selector).action(), # steht für element with id="slider-range"
         $( "#slider-hot" ).slider({
         //setzen der Slider Attributte  
@@ -97,16 +107,53 @@ function setUpHotSlider1(echonest){
         },
         stop: function ( event, ui ) {
                 console.log('Hot slider Stop');
-                if(document.getElementById('song').checked){
+                echonestDynamic.changeArtistHotness();
+                /*if(document.getElementById('song').checked){
                     echonest.getPlaylistSongSimilarity();
             	}else{
             		echonest.getPlaylistArtistSimilarity();
-            	}
+            	}*/
         }      
        
         });
        
         $( "#hot" ).val(  $( "#slider-hot" ).slider( "values", 0 ) +
         " - " + $( "#slider-hot" ).slider( "values", 1 ) );
+}
+
+function setUpSongHotSlider1(echonestDynamic){
+    console.log('SetUpHotSlider() betreten');
+    //jQuery Syntax: $(selector).action(), # steht für element with id="slider-range"
+    $( "#slider-songHot" ).slider({
+    //setzen der Slider Attributte  
+    range: true,
+    min: 0,
+    max: 100,
+    values: [0, 100 ],
+    slide: function( event, ui ) {
+    $( "#songHot" ).val(  ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+    if( ui.values[ 0 ] == ui.values[ 1 ]){
+            //console.log('Slider slide() betreten');
+            $( "#slider-songHot" ).slider( "values", 1,ui.values[ 0 ]+10  );
+    }
+    if( ui.values[ 1 ] == ui.values[ 0 ]){
+            //console.log('Slider slide() betreten');
+            $( "#slider-songHot" ).slider( "values", 0,ui.values[ 1 ]-10  );
+    }
+    },
+    stop: function ( event, ui ) {
+            console.log('SongHot slider Stop');
+            echonestDynamic.changeSongHotness();
+            /*if(document.getElementById('song').checked){
+                echonest.getPlaylistSongSimilarity();
+        	}else{
+        		echonest.getPlaylistArtistSimilarity();
+        	}*/
+    }      
+   
+    });
+   
+    $( "#songHot" ).val(  $( "#slider-songHot" ).slider( "values", 0 ) +
+    " - " + $( "#slider-songHot" ).slider( "values", 1 ) );
 }
 
