b84776418c22e76d745781788273d1631bb74bdc
mark.ormos@gmail.com <mark.ormos@gmail.com@b733beba-dc5d-66aa-b5d8-bccc868f585b>
2013-07-13 04:46:48 +0000

    zwischencommit für playlist. script aufgeräumt und kommentiert

--- trunk/spotify_mrs/scripts/customplaylist.js
+++ trunk/spotify_mrs/scripts/customplaylist.js
@@ -17,8 +17,7 @@ require([
   var setupFlipButton = function(){
 	  var flipbutton = document.getElementById('flipbutton');
 	  flipbutton.onclick=function(){
-	    	console.log("flip button pressed");
-	    	
+	    console.log("flip button pressed");
 	    showPlaylist();
 	  }
   };
@@ -32,23 +31,45 @@ require([
   exports.setupFlipButton = setupFlipButton;
   exports.showPlaylist = showPlaylist;
   
-});
+});//end require
 
-var cnt = 0;
-var playlist;
-var album;
-var list;
+//***************************************************************************************************************
 
+
+var playlist = null;
+var list = null;
+
+/**
+ * Create a new empty playlist.
+ * @param models1 @see spotify api.models
+ * @param List @see spotify views.List
+ */
 function createNewPlaylist1(models1, List){
 	
-	//clearPlaylist(models1);
+	if (playlist != null) clearPlaylist(models1);
 	
+	playlist = null;
 	playlist = models1.Playlist.createTemporary("my new mrs playlist").done(function(playlist){
 		console.log("empty playlist created: ");
 	});
 	
+	playlist.done(function(playlist){
+		if(list == null){
+			list = List.forPlaylist(playlist);
+		    document.getElementById('playlistContainer').appendChild(list.node);
+		    list.init();
+		}
+		
+	});
+	
 }
 
+/**
+ * Add a new track to the playlist. Tracks are provided by the echonestDynamic.js script.
+ * @param List @see spotify views.List
+ * @param models1 @see spotify api.models
+ * @param trackID echonest ID of the track to be added
+ */
 function addTrackToPLaylist1(List, models1, trackID){
 	var id = trackID.replace('-WW','');
 	playlist.done(function(playlist){
@@ -66,20 +87,77 @@ function addTrackToPLaylist1(List, models1, trackID){
 	
 }
 
+/**
+ * Make the playlist visible for the user.
+ * Triggered by user interaction (button click).
+ * Adds the content of the playlist to the List.
+ * @param List @see spotify views.List
+ */
 function showPlaylist1(List){
 	console.log(playlist);
-
 	playlist.done(function(playlist){
-		list = List.forPlaylist(playlist);
-	    document.getElementById('playlistContainer').appendChild(list.node);
-	    list.init();
+		list.clear();
+		list.setItem(playlist);
+		list.init();
 	});
 	
+//	playlist.done(function(playlist){
+//		if(list == null){
+//			list = List.forPlaylist(playlist);
+//		    document.getElementById('playlistContainer').appendChild(list.node);
+//		    list.init();
+//		}
+//		else{
+//			
+//			list.setItem(playlist);
+//			list.init();
+//		}
+//		
+//	});
+//	console.log(list);
 }
 
+/**
+ * Clears the playlist and the list.
+ * Called on new session. 
+ * @param models1 @see spotify api.models
+ */
 function clearPlaylist(models1){
-	models1.Playlist.removeTemporary(playlist).done(function(playlist){
-		console.log("old playlist deleted")
+	
+//	playlist.load("tracks").done(function(playlist){{
+//		playlist.tracks.clear();
+//	}
+	
+//	playlist.done(function(playlist){
+//		playlist.load("tracks").done(function(loadedPlaylist){
+//			loadedPlaylist.tracks.clear();
+//		}
+//	});
+	
+//	if (list!=null) {
+//		playlist.done(function(playlist){
+//			list.destroy();
+//		});
+//	}
+//	
+//	console.log("playlist before remove= "+playlist);
+//	playlist.done(function(playlist){
+//		models1.Playlist.removeTemporary(playlist);
+//	});
+
+	playlist = models1.Playlist.removeTemporary("my new mrs playlist").done(function(playlist){
+		this.playlist = playlist;
+		console.log("playlist deleted");
 	});
+	
+	console.log("playlist after remove= "+playlist);
+//		
+	//list.destroy();
+	console.log(list);
+	
+		
+	
+	
+	
 }
 
