21a6a302b47024d8e603062c1b05ef5b627b4b7f
mark.ormos <mark.ormos@gmail.com>
2013-07-15 22:13:06 +0000

    playlist is cleared when a new session is started

--- spotify_mrs/scripts/customplaylist.js
+++ spotify_mrs/scripts/customplaylist.js
@@ -3,7 +3,6 @@ require([
   '$views/list#List'
 ], function(models, List) {
   'use strict';
-
   
   
   var createNewPlaylist = function() {
@@ -41,6 +40,7 @@ var list = null;
 
 /**
  * Create a new empty playlist.
+ * The playlist is created once, the tracks are deleted on new session.
  * @param models1 @see spotify api.models
  * @param List @see spotify views.List
  */
@@ -49,10 +49,11 @@ function createNewPlaylist1(models1, List){
 	if (playlist != null) clearPlaylist(models1);
 	
 	playlist = null;
-	playlist = models1.Playlist.createTemporary("my new mrs playlist").done(function(playlist){
-		console.log("empty playlist created: ");
+	playlist = models1.Playlist.createTemporary("MRS Playlist").done(function(playlist){
+		console.log("Empty playlist created.");
 	});
 	
+	//bind the playlist to the list when first created
 	playlist.done(function(playlist){
 		if(list == null){
 			list = List.forPlaylist(playlist);
@@ -71,14 +72,16 @@ function createNewPlaylist1(models1, List){
  * @param trackID echonest ID of the track to be added
  */
 function addTrackToPLaylist1(List, models1, trackID){
+	
 	var id = trackID.replace('-WW','');
+	
 	playlist.done(function(playlist){
 		playlist.load("tracks").done(function(loadedPlaylist){
 			var track = models1.Track.fromURI(id);
-			track.load('playable').done(function(track) {
+			track.load('playable', 'name').done(function(track) {
 				if(track.playable){
 					loadedPlaylist.tracks.add(track);
-					console.log("track added to playlist");
+					console.log("Track added to playlist: "+track.name);
 				}
 			});
 			
@@ -94,70 +97,27 @@ function addTrackToPLaylist1(List, models1, trackID){
  * @param List @see spotify views.List
  */
 function showPlaylist1(List){
-	console.log(playlist);
+
 	playlist.done(function(playlist){
 		list.clear();
 		list.setItem(playlist);
 		list.init();
 	});
 	
-//	playlist.done(function(playlist){
-//		if(list == null){
-//			list = List.forPlaylist(playlist);
-//		    document.getElementById('playlistContainer').appendChild(list.node);
-//		    list.init();
-//		}
-//		else{
-//			
-//			list.setItem(playlist);
-//			list.init();
-//		}
-//		
-//	});
-//	console.log(list);
 }
 
 /**
- * Clears the playlist and the list.
+ * Clears the playlist`s tracks list and the list.
  * Called on new session. 
  * @param models1 @see spotify api.models
  */
 function clearPlaylist(models1){
 	
-//	playlist.load("tracks").done(function(playlist){{
-//		playlist.tracks.clear();
-//	}
-	
-//	playlist.done(function(playlist){
-//		playlist.load("tracks").done(function(loadedPlaylist){
-//			loadedPlaylist.tracks.clear();
-//		}
-//	});
-	
-//	if (list!=null) {
-//		playlist.done(function(playlist){
-//			list.destroy();
-//		});
-//	}
-//	
-//	console.log("playlist before remove= "+playlist);
-//	playlist.done(function(playlist){
-//		models1.Playlist.removeTemporary(playlist);
-//	});
-
-	playlist = models1.Playlist.removeTemporary("my new mrs playlist").done(function(playlist){
-		this.playlist = playlist;
+	playlist._args[0].load("tracks").done(function(tracks){
+		playlist._args[0].tracks.clear();
+		list.clear();
 		console.log("playlist deleted");
 	});
-	
-	console.log("playlist after remove= "+playlist);
-//		
-	//list.destroy();
-	console.log(list);
-	
-		
-	
-	
-	
+
 }
 
