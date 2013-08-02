a22b71f00da2b6885349d56882735066056a6f57
mark.ormos@gmail.com <mark.ormos@gmail.com@b733beba-dc5d-66aa-b5d8-bccc868f585b>
2013-08-02 07:05:06 +0000

    offline mode detection part 1

--- trunk/spotify_mrs/scripts/main.js
+++ trunk/spotify_mrs/scripts/main.js
@@ -2,11 +2,10 @@ jQuery.ajaxSettings.traditional = true;
 
 
 require([
-  //'$api/models',
+  '$api/models',
   'scripts/heading',
   'scripts/generatePlaylistButton',
   'scripts/setupSlider',
-
   'scripts/echonestDynamic',
   'scripts/echonest',
   'scripts/setupGenreFilter',
@@ -18,71 +17,79 @@ require([
   'scripts/setupNoveltyCheckBoxes',
   'scripts/playlistInformation'
   
-  
-  
-
-  // generatePlaylistButton,
-], function( heading,generatePlaylistButton,setupSlider, echonestDynamic, echonest, setupGenreFilter,setupTagCloud, setUpSimilarityAccordion, echonestTasteProfile, customplaylist, jPagesSetup, setupNoveltyCheckBoxes, playlistInformation) {
+], function( models, heading,generatePlaylistButton,setupSlider, 
+		echonestDynamic, echonest, setupGenreFilter,setupTagCloud, 
+		setUpSimilarityAccordion, echonestTasteProfile, customplaylist, 
+		jPagesSetup, setupNoveltyCheckBoxes, playlistInformation) {
   'use strict';
   
  
+	  models.session.addEventListener('change', changeOffline);
+	  
+	  function changeOffline(){
+		  models.session.load('online').done(function(session){
+			  console.log(models.session.online);
+		  });
+	  }
+  
+	  $(document).ready(function(){
+		
+		  models.session.load('online').done(function(session){
+			  var online = models.session.online;
+			  
+			  if(online){
+				  console.log("app is online");
+				  init();
+			  }
+			  else if(!online){
+				  console.log("app is offline");
+			  }
+		  });
+	
+	  }); 
   
   
   
-  $(document).ready(function(){
-	  
-	  //console.log('Main() executed');
+	  function init(){
+		// heading.writeHeading();
+		//echonestTasteProfile.createTasteProfile();
 
-	// heading.writeHeading();
-	  
+		playlistInformation.setUpPlaylistInformation();
+
+		setupNoveltyCheckBoxes.setUpExcludeSeedArtistCheckBox();
+		setupNoveltyCheckBoxes.setUpNoSongsFromSpotifyCollectionCheckBox();
+
+		generatePlaylistButton.setUpNewSeedButton();
+		generatePlaylistButton.setUpNextSongsButton();
+
+		setupSlider.setUpPopSlider(echonestDynamic);
+		setupSlider.setUpHotSlider(echonestDynamic);
+		setupSlider.setUpSongHotSlider(echonestDynamic);
+		setupSlider.setUpArtistVarietySlider(echonestDynamic);
+		setupSlider.setUpAdventurousnessSlider(echonestDynamic);
+
+		jPagesSetup.setupPages();
+
+		echonestDynamic.startNewSession();
+
+		setupGenreFilter.setupGenreFilter(echonestDynamic);
+		setUpSimilarityAccordion.setupAccordion();
+
+		setUpSimilarityAccordion.setupChangeSeedArtistButton();
+
+		setUpSimilarityAccordion.setupChangeSeedSongButton();
+
+		customplaylist.setupFlipButton();
+		customplaylist.createNewPlaylist();
+
+		//setupTagCloud.addTagCloudEventHandler(echonestDynamic);
+		// generatePlaylistButton.setUpNewSeedButton();
+		// generatePlaylistButton.setUpGeneratePlaylistButton();
+		// setupSimilarityRadioButtons.setupSimilarityButtons();
+		//setupSlider.setUpPopSlider(echonest);
+		// setupSlider.setUpHotSlider(echonest);
+		 
+	  }
 	
-	 
-	 //echonestTasteProfile.createTasteProfile();
-	  
-	  playlistInformation.setUpPlaylistInformation();
-	  
-	  setupNoveltyCheckBoxes.setUpExcludeSeedArtistCheckBox();
-	  setupNoveltyCheckBoxes.setUpNoSongsFromSpotifyCollectionCheckBox();
-	 
-	  generatePlaylistButton.setUpNewSeedButton();
-	  generatePlaylistButton.setUpNextSongsButton();
-	  
-	 setupSlider.setUpPopSlider(echonestDynamic);
-	 setupSlider.setUpHotSlider(echonestDynamic);
-	 setupSlider.setUpSongHotSlider(echonestDynamic);
-	 setupSlider.setUpArtistVarietySlider(echonestDynamic);
-	 setupSlider.setUpAdventurousnessSlider(echonestDynamic);
-	 
-	 jPagesSetup.setupPages();
-	 
-	 echonestDynamic.startNewSession();
-	 
-	 setupGenreFilter.setupGenreFilter(echonestDynamic);
-	 setUpSimilarityAccordion.setupAccordion();
-	 
-	 setUpSimilarityAccordion.setupChangeSeedArtistButton ();
-	 
-	 setUpSimilarityAccordion.setupChangeSeedSongButton();
-	 
-	 customplaylist.setupFlipButton();
-	 customplaylist.createNewPlaylist();
-	 
-	 //setupTagCloud.addTagCloudEventHandler(echonestDynamic);
-	  
 	
-	 
-	
-	  
-	 // generatePlaylistButton.setUpNewSeedButton();
-	 // generatePlaylistButton.setUpGeneratePlaylistButton();
-	  
-	 // setupSimilarityRadioButtons.setupSimilarityButtons();
-	  
-	  //setupSlider.setUpPopSlider(echonest);
-	 // setupSlider.setUpHotSlider(echonest);
-	 
-
-	}); 
-
-
 });
